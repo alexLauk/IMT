1a5ee6aadf8dfb8bef86b2a745669a5a68be3307
alexLauk <aleksandr.romanko.dev@gmail.com>
2019-10-06 22:47:18 +0300

    добавил комменты

--- 14/app.js
+++ 14/app.js
@@ -1,7 +1,6 @@
 const http = require('http');
 const fs = require('fs');
 const request = require('request');
-const currUrl = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR28vaK18w5_ZDwhGTzyv4PCL0qA58u3ISu6Na5ntYWFA788MHC0cp3fkww'
 
 http.createServer(function(req, res){
 
@@ -20,17 +19,18 @@ http.createServer(function(req, res){
     }
     else if(req.url === '/currency'){
 
+        // cоздаем GET запрос
         request({
             method: 'GET',
-            url: currUrl,
-            qs: {
-              param: 'edit',
-              value: 100
-            }
-            }, function (error, response, body) {
+            url: 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=3&fbclid=IwAR28vaK18w5_ZDwhGTzyv4PCL0qA58u3ISu6Na5ntYWFA788MHC0cp3fkww'
+            },
 
+            function (error, response, body) {
+
+            // анализируем HTTP-статус ответа
             if (!error && response.statusCode == 200) {
 
+                // если всё ОК, получаем содержимое файла
                 fs.readFile('index.html', 'utf8', function(err, data){
 
                     if(err){
@@ -38,10 +38,12 @@ http.createServer(function(req, res){
                         res.end('File not found!');
                     }
                     else{
+                        // если всё ОК, парсим полученный ответ
                         curr = JSON.parse(body);
 
                         let currency =[];
 
+                        // меняем плейсхолдер из файла на полученные данные из response с помощью метода data.replace().
                         for(let i=0; i<curr.length; i++){
 
                              currency.push('<p>Currency: ' + curr[i].ccy + ' - UAH<p>'+'\n<p>Buy: ' + curr[i].buy + '<p>'+'\n<p>Sale: ' + curr[i].sale + '<p>');
