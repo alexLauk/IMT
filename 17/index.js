ac4257f7010de21de79d76b00515556fadfbec24
alexLauk <aleksandr.romanko.dev@gmail.com>
2019-10-21 21:00:30 +0300

    опитимизировал пакет валидации полей
    сделал единый метод validation, который позволяет провалидировать все поля

--- 17/index.js
+++ 17/index.js
@@ -1,4 +1,4 @@
-const validator = require('./validator');
+const fvp = require('./fvp');
 const express = require('express');
 const bodyParser = require('body-parser');
 //const valid = validator();
@@ -10,19 +10,19 @@ app.get('/', urlencodedParser, function (request, response) {
     response.sendFile(__dirname + '/index.html');
 });
 
-app.post('/validate', urlencodedParser, function (request, response) {
+app.post('/validation', urlencodedParser, function (request, response) {
     if(!request.body) return response.sendStatus(400);
 
     let responseToUser =[];
    //use regexp;
-   if(!validator.name(request.body.userName)) responseToUser.push(`<p>Оnly letters, may consist of several words with spaces and contain at least 3 characters</p>`);
+   if(!fvp.validation(request.body.userName, 'name')) responseToUser.push(`<p>Оnly letters, may consist of several words with spaces and contain at least 3 characters</p>`);
 
-   if(!validator.email(request.body.userEmail)) responseToUser.push(`<p>Mail (4-10 characters + @ + 4-10 characters +. + at least 2 characters)
+   if(!fvp.validation(request.body.userEmail,'email')) responseToUser.push(`<p>Mail (4-10 characters + @ + 4-10 characters +. + at least 2 characters)
    </p>`);
 
-   if(!validator.mob(request.body.userFhone)) responseToUser.push(`<p>Enter the phone in the format +380 ___ __ __</p>`);
+   if(!fvp.validation(request.body.userFhone,'mob')) responseToUser.push(`<p>Enter the phone in the format +380 ___ __ __</p>`);
 
-   if(!validator.pass(request.body.userPassword)) responseToUser.push(`<p>Password more than 5 characters,
+   if(!fvp.validation(request.body.userPassword,'pass')) responseToUser.push(`<p>Password more than 5 characters,
    contains at least one number, one uppercase letter, one lowercase letter and at least one character that is not alphanumeric</p>`);
 
    response.send(responseToUser.join(''));
